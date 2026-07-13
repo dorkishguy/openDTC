ad1d21458f2960b5f23681a0934d31bbea80cb2f
dorkishguy <ameenfaquiih@gmail.com>
2026-07-13 22:00:01 +0530

    trying to implement pb instead of taking from static data

--- backend/get_bus.py
+++ backend/get_bus.py
@@ -1,33 +1,27 @@
-import sqlite3
+from google.transit import gtfs_realtime_pb2
+import requests
 import sys
-import datetime
+import requests
+from dotenv import load_dotenv
+import os
 
-conn = sqlite3.connect("static.db")
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
-c = conn.cursor()
+# if len(sys.argv) == 3:
+# bus = sys.argv[1]
+# stop = sys.argv[2]
+feed = gtfs_realtime_pb2.FeedMessage()
+response = requests.get(f'https://otd.delhi.gov.in/api/realtime/VehiclePositions.pb?key={API_KEY}')
+response = feed.ParseFromString(response.content)
+print(response)
+for entity in feed.entity:
+  print(entity)
 
-if len(sys.argv) == 2:
-    now = str(datetime.datetime.now()).split()[1].split(".")[0].split(":")
-    now = datetime.time(int(now[0]), int(now[1]), int(now[2]))
-    bus = sys.argv[1]
-    c.execute(f"SELECT * FROM routes WHERE route_long_name LIKE '%{bus}%'")
-    print(c.fetchall(), "\n \n")
-    # c.execute(f"SELECT * FROM trips WHERE route_id = {c.fetchall()[0][1]}")
-    # print(c.fetchall(), "\n \n")
-    # c.execute(f"SELECT * FROM stop_times WHERE trip_id = '{c.fetchone()[2]}'")
-    # print(c.fetchall(), "\n \n")
-    data = c.fetchall()
-    arrivals = [datas[1] for datas in data]
-    # for arrival in arrivals:
-    #     arrival = arrival.split(":")
-    #     arrival = datetime.time(int(arrival[0]), int(arrival[1]), int(arrival[2]))
-    #     if arrival < now:
-    #         print(f"{arrival} < {now}")
-    #         pass
-    #     else:
-    #         print(f"Next bus on {arrival}")
 
-elif len(sys.argv) < 2:
-    print("add bus code")
-elif len(sys.argv) > 2: 
-    print("improper bus code")
+# elif len(sys.argv) < 3:
+#     print("add bus code")
+# elif len(sys.argv) == 2: 
+#     print("add stop name")
+# elif len(sys.argv) == 1:
+#     print("add bus code and stop")
